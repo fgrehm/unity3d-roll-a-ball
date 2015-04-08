a7c3f3759aca036c250a9b002162e3324837dfdf
Fabio Rehm <fgrehm@gmail.com>
2015-04-08 03:11:23 -0300

    End of http://unity3d.com/learn/tutorials/projects/roll-a-ball/moving-the-player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	// Before physics calculations
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+}
