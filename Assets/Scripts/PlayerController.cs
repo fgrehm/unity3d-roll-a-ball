28b3b8ad3dae07436c4fc8ffdf112e51fa57ad09
Fabio Rehm <fgrehm@gmail.com>
2015-04-08 04:03:49 -0300

    [COLLECTING AND COUNTING] Collecting cubes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,4 +13,9 @@ public class PlayerController : MonoBehaviour {
 
 		rigidbody.AddForce(movement * speed * Time.deltaTime);
 	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp")
+			other.gameObject.SetActive(false);
+	}
 }
