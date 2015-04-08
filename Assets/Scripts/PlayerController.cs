5dbf82a74a332ed5f22d914abf8d5bc26e21ba95
Fabio Rehm <fgrehm@gmail.com>
2015-04-08 10:23:29 -0300

    Done with http://unity3d.com/learn/tutorials/projects/roll-a-ball/displaying-text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
+	public Text countText;
+	public Text winText;
+	private int count;
+
+	void Start() {
+		count = 0;
+		//countText = GetComponent<Text>();
+		SetCountText();
+		winText.text = "";
+	}
 
 	// Before physics calculations
 	void FixedUpdate() {
@@ -15,7 +26,17 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag == "PickUp")
-			other.gameObject.SetActive(false);
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString();
+		if (count >= 12) {
+			winText.text = "YOU WIN!";
+		}
 	}
 }
